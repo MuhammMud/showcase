3f2a09ba358700379c98b38a5ff0db8cd38c4adb
Muhammad Mudassir <104167723+MuhammMud@users.noreply.github.com>
2022-07-01 16:39:49 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { Download, Features, SectionWrapper } from './components';
+import assets from './assets';
+import styles from './styles/Global';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <SectionWrapper 
+        title="Your own store of NFTs. Start Selling & Growing"
+        description="Buy, collect, exchange & earn NFTs. Join 5+ million people using this growing Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SectionWrapper 
+        title="Smart User Interface Marketplace"
+        description="Experience the smooth UI of EthBloc Marketplace. Consistent colors built using a fluent and integrated UI design."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SectionWrapper 
+        title="Deployment"
+        description="EthBloc is built using Expo which runs natively on all users' devices. Easily allowing you to get your app out in the world."
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper 
+        title="A new way to showcase the store"
+        description="The app contains two screens. The first shows a list of all NFTs while the second focuses on the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made by {" "}
+        <span className="bold">MMudassir</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Download.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import styles from '../styles/Global';
+import assets from '../assets';
+
+const Download = () => {
+  return (
+    <div className={`${styles.section} ${styles.bgWhite}`}>
+      <div className={`${styles.subSection} flex-col text-center`}>
+        <div>
+          <h1 className={`${styles.h1Text} ${styles.blackText}`}>Check out the behind the scenes</h1>
+          <p className={`${styles.pText} ${styles.blackText}`}>Get the source code on my GitHub</p>
+        </div>
+        <button className={styles.btnPrimary}>
+            <a href='https://github.com/MuhammMud/react_native_nftapp' target="_blank">Enjoy!!</a>
+        </button>
+        <div className={styles.flexCenter}>
+          <img 
+            src={assets.scene}
+            alt="download_png"
+            className={styles.fullImg}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Download
